3f235a0c0f60b502ae36ad83a12270fbab458ba5
Andrii Tsurkan <andrii.tsurkan@dxc.com>
2022-10-16 17:38:09 +0300

    Home work to Lesson7_Arrays

--- Program.cs
+++ Program.cs
@@ -1,50 +1,28 @@
-﻿using System;
+﻿{
+int n = 5;
+Console.WriteLine($"Factorial ({n}) = {Factorial (n)}");
+Console.WriteLine($"Method ({n}) = {Method (n)}");
+Console.WriteLine($"Fibonachi ({n}) = {Fibonachi (n)}");
 
+
+
+
+int Factorial(int n)
 {
+    if (n == 1) return 1;
+ 
+    return n * Factorial(n - 1);
+}
 
-long longVar = 987654321098787;
-int integerVar = 56565;
-short shortVar = 1234;
-byte byteVar = 127;
-bool boolType = false;
-char oneChar = 'a';
-float floatVar = 543.654F;
-double doubleVar = 987124.12D;
-decimal decimalVar = 20.001M;
+int Method(int num) => num > 0 ? num + Method(num - 1) : 1;
 
-long substraction;
-short sum;
-double multiplying;
-double dividing;
+int Fibonachi (int n)
+{
+    if (n == 0 || n== 1) 
+	return 1;
+	else
+    return (n-1)  +  (n - 2);
+}
 
-substraction = longVar - integerVar;
-sum = Convert.ToInt16(shortVar + byteVar);
-multiplying = doubleVar*floatVar;
-dividing = doubleVar/floatVar;
-
-Console.WriteLine("substraction is equal to: " + substraction);
-Console.WriteLine("sum is equal to: " + sum);
-Console.WriteLine("multiplying is equal to: " + multiplying);
-Console.WriteLine("dividing is equal to: " + dividing);
-Console.WriteLine();
-
-
-double x = -1800d;
-double y = 500d;
-double firstTask;
-double secondTask;
-double thirdTask;
-double fourthTask;
-
-
-firstTask = Convert.ToDouble(-6*Math.Pow(x,3) + 5*Math.Pow(x,2) - 10*x + 15);
-secondTask = Convert.ToDouble(Math.Abs(x)*Math.Sin(x));
-thirdTask = Convert.ToDouble(2*Math.PI*x);
-fourthTask = Math.Max(x, y);
-
-Console.WriteLine("firstTask is equal to: " + firstTask);
-Console.WriteLine("secondTask is equal to: " + secondTask);
-Console.WriteLine("thirdTask is equal to: " + thirdTask);
-Console.WriteLine("fourthTask is equal to: " + fourthTask);
 
 }
