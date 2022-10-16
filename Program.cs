17e9b6793368dd2d8cd41a532351cb19fbeb941c
Andrii Tsurkan <andrii.tsurkan@dxc.com>
2022-10-16 18:14:45 +0300

    Home task to Lesson 7 "Arrays"

--- Program.cs
+++ Program.cs
@@ -1,50 +1,194 @@
-﻿using System;
+﻿/*
+Task:
 
+Implement 3 sort algorithms:
+
+Selection
+Bubble
+Insertion
+
+Extra:
+
+Define enum SortAlgorithmType with all 3 algorithms types and create single function Sort that will accept array and SortAlgorithmType and use passed algorithm to sort array
+
+Define enum OrderBy with 2 values: Asc and Desc and update Sort method with this parameter that will change sort order
+*/
+
+
+
+
+int[] numArray = new[] {12, 22, 98, 20, 78, 556, -4, 0, -2}; //Setting array to be sorted
+
+// Invoking each sorting method separately
+
+selectionSort(numArray);
+bubbleSort(numArray);
+insertionSort(numArray);
+
+//Sorting with Acs order as default
+sort(SortAlgorithmType.bubbleSort, numArray);
+
+//Sorting with ability to define order type Asc or Desk. Order types are implemented in printArray(), printArrayDesc methods, respectively. 
+sort(SortAlgorithmType.insertSort, numArray, AscOrderBy.Desc);
+
+//Sorting with ability to define order type Asc or Desk. Desk order type is implemented with revertArray() method. 
+sortAndRevert(SortAlgorithmType.selectionSort, numArray, AscOrderBy.Desc);
+
+static int[] selectionSort (int[] arr)
 {
+    int n = arr.Length;
+    for (int i = 0; i < n - 1; i++)
+    {
+        int min_idx = i;
+        for (int j = i + 1; j < n; j++)
+            if (arr[j] < arr[min_idx])
+                min_idx = j;
 
-long longVar = 987654321098787;
-int integerVar = 56565;
-short shortVar = 1234;
-byte byteVar = 127;
-bool boolType = false;
-char oneChar = 'a';
-float floatVar = 543.654F;
-double doubleVar = 987124.12D;
-decimal decimalVar = 20.001M;
+        int temp = arr[min_idx];
+        arr[min_idx] = arr[i];
+        arr[i] = temp;
+    }
+    return arr;
+}
 
-long substraction;
-short sum;
-double multiplying;
-double dividing;
+static int[] bubbleSort(int[] arr)
+{
+    int n = arr.Length;
+    for (int i = 0; i < n - 1; i++)
+        for (int j = 0; j < n - i - 1; j++)
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+    return arr;
+}
 
-substraction = longVar - integerVar;
-sum = Convert.ToInt16(shortVar + byteVar);
-multiplying = doubleVar*floatVar;
-dividing = doubleVar/floatVar;
+static int[] insertionSort(int[] arr)
+{
+    int n = arr.Length;
+    for (int i = 1; i < n; ++i)
+    {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+    return arr;
+}
 
-Console.WriteLine("substraction is equal to: " + substraction);
-Console.WriteLine("sum is equal to: " + sum);
-Console.WriteLine("multiplying is equal to: " + multiplying);
-Console.WriteLine("dividing is equal to: " + dividing);
-Console.WriteLine();
+void sort (SortAlgorithmType algorithmType, int [] arr, AscOrderBy order = AscOrderBy.Asc)
+{
+ string sortType = "";
+    switch (algorithmType)
+    {
+        case SortAlgorithmType.selectionSort:
+            selectionSort(arr);
+            sortType = "Selection Sort";
+            break;
+        case SortAlgorithmType.bubbleSort:
+            bubbleSort(arr);
+            sortType = "Bubble Sort";
+            break;
+        case SortAlgorithmType.insertSort:
+            insertionSort(arr);
+            sortType = "Insertion Sort";
+            break;
+    }
 
+Console.Write(sortType + ": ");
 
-double x = -1800d;
-double y = 500d;
-double firstTask;
-double secondTask;
-double thirdTask;
-double fourthTask;
+    switch (order)
+    {
+        case AscOrderBy.Asc:
+            Console.Write("Order Asc: ");
+            printArray(arr);
+            break;
+        case AscOrderBy.Desc:
+            Console.Write("Order Desc: ");
+            printArrayDesc(arr);
+            break;
+    }
+}
 
+void sortAndRevert(SortAlgorithmType algorithmType, int[] arr, AscOrderBy order)
+{
+    string sortType = "";
+    switch (algorithmType)
+    {
+        case SortAlgorithmType.selectionSort:
+            selectionSort(arr);
+            sortType = "Selection Sort";
+            break;
+        case SortAlgorithmType.bubbleSort:
+            bubbleSort(arr);
+            sortType = "Bubble Sort";
+            break;
+        case SortAlgorithmType.insertSort:
+            insertionSort(arr);
+            sortType = "Insertion Sort";
+            break;
+    }
 
-firstTask = Convert.ToDouble(-6*Math.Pow(x,3) + 5*Math.Pow(x,2) - 10*x + 15);
-secondTask = Convert.ToDouble(Math.Abs(x)*Math.Sin(x));
-thirdTask = Convert.ToDouble(2*Math.PI*x);
-fourthTask = Math.Max(x, y);
+    Console.Write(sortType + ": ");
 
-Console.WriteLine("firstTask is equal to: " + firstTask);
-Console.WriteLine("secondTask is equal to: " + secondTask);
-Console.WriteLine("thirdTask is equal to: " + thirdTask);
-Console.WriteLine("fourthTask is equal to: " + fourthTask);
+    switch (order)
+    {
+        case AscOrderBy.Asc:
+            Console.Write("Order Asc: ");
+            printArray(arr);
+            break;
+        case AscOrderBy.Desc:
+            arr = revertArray(arr);
+            Console.Write("Order Desc: ");
+            printArray(arr);
+            break;
+    }
+}
 
+static void printArray(int[] arr)
+{
+    int n = arr.Length;
+    for (int i = 0; i < n; ++i)
+        Console.Write(arr[i] + " ");
+    Console.WriteLine();
+}
+
+static void printArrayDesc(int[] arr)
+{
+    int n = arr.Length -1;
+    for (int i = n; i >= 0; --i)
+        Console.Write(arr[i] + " ");
+    Console.WriteLine();
+}
+
+static int[] revertArray(int[] arr)
+{
+    int n = arr.Length;
+    int temp;
+    for(int i = 0; i < n/2; i++)
+    {
+        temp = arr[n-i-1];
+        arr[n - i - 1] = arr[i];
+        arr[i] = temp;
+    }
+    return arr;
+}
+
+enum SortAlgorithmType
+{
+    selectionSort,
+    bubbleSort,
+    insertSort
+}
+
+enum AscOrderBy
+{
+    Asc,
+    Desc
 }
