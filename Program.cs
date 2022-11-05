7765de4845dd0b5d560a702f19bd4cc1fd7951f7
Andrii Tsurkan <andrii.tsurkan@dxc.com>
2022-11-05 17:23:57 +0200

    HomeTask9_Lesson12_Encapsulation

--- Program.cs
+++ Program.cs
@@ -1,173 +1,29 @@
 ﻿/*
 Task:
-
-Provide ability to search in phone book by any criteria (first/last name or phone number)
-
-Extra:
-
-Update phone number program that will store rows in alphabetical order (order by last name, first name and then phone number)
-
-Use binary search algorithm to search for row in phone book
+Finish describing ‘school’ domain. Define methods to add new pupils, organize classes, create schedule, update teacher’s information
 
 */
 
+using School_project;
 
+string firstName = null;
+string lastName = null;
+string classId = null;
+string pupilId = null;
+string userName = null;
+string gender = null;
+string pathPupil = "Pupils.csv";
+string pathTeacher = "Teacher.csv";
 
-using System.IO;
-using System.Text.RegularExpressions;
-using System.Threading.Channels;
+Pupil pupil = new Pupil(firstName, lastName, classId, pupilId, userName, gender);
 
+var classList = Pupil.ReadCsvFile(pathPupil);
 
-var pathToPhonebook = "MyPhonebook.csv";
+Pupil.AddOrViewPupilData(classList, pathPupil); // Add or View pupil data by Pupil Id or by Class Id
 
-var phoneBook = ReadCsvFile(ref pathToPhonebook); //read data from phonebook file and pack them to list
+Teacher.CreateNewTeacher(pathTeacher); 
 
-SelectOperation(phoneBook, ref pathToPhonebook);
+Teacher.UpdateTeacherById(pathTeacher);
 
-OrderContactsBy(phoneBook);
-
-
-
-static void SelectOperation(List<(string Firstname, string Secondname, string Phonenumber, string Group)> phoneList, ref string pathToPhonebook)
-{
-    Console.WriteLine($"Select operation. " +
-        $"\n Press '1' and Enter - Input new entry in phonebook; " +
-        $"\n Press '2' and Enter - Search contact by any criteria " +
-        $"\n Press '3' and Enter - Skip input  and pass to sorting phonebook" +
-        $"\n Press '4' and Enter - Cancel input");
-
-    string choiceSelect = Console.ReadLine();
-    if (choiceSelect == "1")
-    {
-        string inputContact = InputValue(); // introduction to input new contact
-        ConfirmInput(inputContact, ref pathToPhonebook); // creation one contact in the list and file
-        var contactList = ReadCsvFile(ref pathToPhonebook);
-        PrintPhonebook(contactList, ref pathToPhonebook);
-    }
-    else if (choiceSelect == "2")
-    {
-        SearchContactsBy(searchInput(), phoneList);
-    }
-    else if (choiceSelect == "3")
-    {
-        Console.WriteLine("Input and Search are skipped. Move to sorting method.");
-    }
-    else if (choiceSelect == "4")
-    {
-        CancelInput();
-    }
-}
-
-
-static List<(string Firstname, string Secondname, string Phonenumber, string Group)> ReadCsvFile(ref string path)
-{
-    if (!File.Exists(path)) return null;
-    var bookList = new List<(string Firstname, string Secondname, string Phonenumber, string Group)>();
-    var rows = File.ReadAllLines(path);
-    for (int i = 1; i < rows.Count(); i++)
-    {
-        if (rows[i].Length != 0)
-        {
-            var splitComma = rows[i].Split(",");
-            bookList.Add((splitComma[0], splitComma[1], splitComma[2], splitComma[3]));
-        }
-    }
-    return bookList;
-}
-
-static void PrintPhonebook(List<(string Firstname, string Secondname, string Phonenumber, string Group)> contacts, ref string path)
-{
-    Console.WriteLine($"Read data from {path}:");
-    Console.WriteLine("Firstname - Secondname - Phonenumber - Group");
-
-    foreach (var row in contacts)
-        Console.WriteLine("{0}, {1}, {2}, {3}", row.Firstname, row.Secondname, row.Phonenumber, row.Group);
-}
-
-static string InputValue()
-{
-    Console.WriteLine("Enter contact data separated by comma: Firstname,Secondname,Phonenumber,Group");
-    string input = Console.ReadLine();
-    Console.WriteLine($"Input: {input}");
-    return input;
-}
-
-static void ConfirmInput(string inputData, ref string path)
-{
-    var confirmInvitation = "Please confirm Input. Press Y - yes or N - no";
-    string choice;
-    do
-    {
-        Console.WriteLine(confirmInvitation);
-        choice = Console.ReadLine();
-        if (choice == "N" || choice == "Y")
-            break;
-    }
-    while (choice != "N" || choice != "Y");
-    {
-        if (choice == "Y") { AddContact(inputData, ref path); }
-        else { CancelInput(); }
-    }
-}
-
-static void CancelInput()
-{
-    Console.WriteLine("Input rejected by user");
-    Environment.Exit(1);
-}
-
-static void AddContact(string input, ref string path)
-{
-    File.AppendAllLines(path, new[] { $"\n{input}" });
-    Console.WriteLine($"Contact \"{input}\" has been added in the file \"{path}\"");
-}
-
-static List<(string Firstname, string Secondname, string Phonenumber, string Group)> SearchContactsBy(string input, List<(string Firstname, string Secondname, string Phonenumber, string Group)> collection)
-{
-    var searchResult = new List<(string Firstname, string Secondname, string Phonenumber, string Group)>();
-    for (int row = 0; row < collection.Count; row++)
-    {
-        if (collection[row].Firstname.Contains(input, StringComparison.OrdinalIgnoreCase) || collection[row].Secondname.Contains(input, StringComparison.OrdinalIgnoreCase) || collection[row].Phonenumber.Contains(input))
-            searchResult.Add(collection[row]);
-    }
-    if (searchResult.Count != 0)
-    {
-        foreach (var row in searchResult)
-            Console.WriteLine("{0}, {1}, {2}", row.Firstname, row.Secondname, row.Phonenumber);
-    }
-    else { Console.WriteLine($"Nothing found for \"{input}\""); }
-    return searchResult;
-}
-
-static string searchInput()
-{
-    string search;
-    string inputInvitation = "Enter search word or number and press Enter";
-    do
-    {
-        Console.WriteLine(inputInvitation);
-        search = Console.ReadLine();
-    }
-    while (search.Length == 0);
-    return search;
-}
-
-static List<(string Firstname, string Secondname, string Phonenumber, string Group)> OrderContactsBy(List<(string Firstname, string Secondname, string Phonenumber, string Group)> collection)
-{
-    string newPath = "MyPhonebook_Ordered.csv";
-    var collectionOrdered = collection.OrderBy(c => c.Secondname).ThenBy(c => c.Firstname).ThenBy(c => c.Phonenumber).ToList();
-    File.Delete(newPath);
-
-    foreach (var row in collectionOrdered)
-    {
-        File.AppendAllLines(newPath, new[] { $"\n{row}" }); 
-    }
-
-    var contactList = ReadCsvFile(ref newPath);
-    Console.WriteLine();
-    Console.WriteLine("Sorted by last name, first name and then phone number:");
-    PrintPhonebook(contactList, ref newPath);
-    return contactList;
-}
 
 
---
+++ Pupil.cs
@@ -0,0 +1,178 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School_project;
+
+public class Pupil
+{
+    string PupilId;
+    string ClassId;
+    string FirstName;
+    string LastName;
+    string Description;
+    string UserName;
+    string UserId;
+    string Email { get; set; }
+    string Login { get; set; }
+    string Password { get; set; }
+    string Address { get; set; }
+
+    static Pupil()
+    {}
+
+    public Pupil(string fistName, string lastName, string classId, string pupilId, string userName, string gender)
+    {}
+
+    public static void AddOrViewPupilData (List<(string FirstName, string LastName, string UserName, string ClassId, string PupilId, string Gender)> classList, string pathToFile)
+    {
+        Console.WriteLine($"Add or View pupil data? 1 - add; 2 - view Pupil by Pupil Id; 3- view Pupils of class by Class Id");
+        var input = Console.ReadLine();
+        if (input == "1")
+        {
+            classList = AddPupilToClassList(classList, pathToFile);
+        }
+        else if (input == "2")
+        {
+            Console.WriteLine($"Input Pupil Id");
+            var inputId = Console.ReadLine();
+            Pupil.PrintPupilDataByPupilId(classList, inputId);
+        }
+
+        else if (input == "3")
+        {
+            Console.WriteLine($"Input Class Id");
+            var inputId = Console.ReadLine();
+            Pupil.PrintClassListByClassId(classList, inputId);
+        }
+
+        else
+        {
+            Console.WriteLine("Not corect input");
+            Environment.Exit(1);
+        }
+    }
+
+    public static  List<(string FirstName, string LastName, string UserName, string ClassId, string PupilId, string Gender)> AddPupilToClassList (List<(string FirstName, string LastName, string UserName, string ClassId, string PupilId, string Gender)> classList, string pathToFile)
+    {
+        (string FirstName, string LastName, string UserName, string ClassId, string PupilId, string Gender) pupilDataList = InputPupilDataAddToClass(pathToFile);
+        classList.Add(pupilDataList);
+
+        return classList;
+    }
+
+    static (string FirstName, string LastName, string UserName, string ClassId, string PupilId, string Gender) InputPupilDataAddToClass(string path)
+    {
+        (string FirstName, string LastName, string UserName, string ClassId, string PupilId, string Gender) inputData = (null, null, null, null, null, null);
+        string pathFile = path;
+
+        Console.WriteLine("Enter FirstName:");
+        var firstName = Console.ReadLine();
+        Console.WriteLine($"First name submitted: {firstName}");
+        inputData.FirstName = firstName;
+        Console.WriteLine("Enter LastName:");
+        var lastName = Console.ReadLine();
+        Console.WriteLine($"Second name submitted: {lastName}");
+        inputData.LastName = lastName;
+        var userName = firstName + "." + lastName;
+        Console.WriteLine($"UserName is : {userName}");
+        inputData.UserName = userName;
+        Console.WriteLine("Enter ClassId:");
+        var classId = Console.ReadLine();
+        Console.WriteLine($"ClassId submitted: {classId}");
+        inputData.ClassId = classId;
+        Console.WriteLine("Enter PupilId:");
+        string pupilId = Console.ReadLine();
+        inputData.PupilId = pupilId;
+        Console.WriteLine("Enter gender: male; female");
+        var genderInput = Console.ReadLine();
+        inputData.Gender = genderInput;
+        Console.WriteLine($"Gender submitted: {genderInput}");
+
+        AddPupilToFile(inputData, pathFile);
+
+        return inputData;
+    }
+
+    public static void PrintPupilDataByPupilId(List<(string FirstName, string LastName, string UserName, string ClassId, string PupilId, string Gender)> classList, string pupilId)
+    {
+        int count = 0;
+
+        Console.WriteLine($"Print data for pupilId: {pupilId}");
+
+        foreach (var row in classList)
+        {
+            if (row.PupilId == pupilId)
+            {
+                count++;
+                Console.WriteLine("First name: " + row.FirstName);
+                Console.WriteLine("Last name: " + row.LastName);
+                Console.WriteLine("User name: " + row.UserName);
+                Console.WriteLine("Class Id: " + row.ClassId);
+                Console.WriteLine("Pupil Id: " + row.PupilId);
+                Console.WriteLine("Gender: " + row.Gender);
+            }
+        }
+        if (count == 0)
+        {
+            Console.Write("Data not found");
+            Environment.Exit(1);
+        }
+    }
+
+    static void PrintClassListByClassId(List<(string FirstName, string LastName, string UserName, string ClassId, string PupilId, string Gender)> classList, string classId)
+    {
+        int count = 0;
+        Console.WriteLine($"Data for classId: {classId}");
+        Console.WriteLine("Firstname - Lastname - UserName - ClassId - PupilId - Gender:");
+
+        if (classList.Count != 0)
+        {
+            for (int i = 0; i < classList.Count; i++)
+            {
+                if (classList[i].ClassId == classId)
+                {
+                    ++count;
+                    Console.WriteLine($"{classList[i].FirstName}, {classList[i].LastName}, {classList[i].UserName}, {classList[i].ClassId}, {classList[i].PupilId}, {classList[i].Gender}");
+                    continue;
+                }
+            }
+            if (count == 0)
+                Console.WriteLine("Data not found");
+            else
+                Console.WriteLine(($"Found {count} entries"));
+        }
+        else if (classList.Count == 0)
+            Console.WriteLine("classList collection is empty");
+    }
+
+    static void AddPupilToFile((string Firstname, string Lastname, string UserName, string ClassId, string PupilId, string Gender) input, string path)
+    {
+        File.AppendAllLines(path, new[] {  $" {input.Firstname},  {input.Lastname}, {input.UserName}, {input.ClassId}, {input.Firstname}, {input.PupilId}, {input.Gender}"});
+        Console.WriteLine($"Contact \"{input}\" has been added in the file \"{path}\"");
+    }
+
+    public static List<(string Firstname, string Lastname, string UserName, string ClassId, string PupilId, string Gender)> ReadCsvFile(string path)
+    {
+        if (!File.Exists(path)) return null;
+        var classList = new List<(string Firstname, string Lastname, string UserName, string ClassId, string PupilId, string Gender)>();
+        var rows = File.ReadAllLines(path);
+        for (int i = 1; i < rows.Count(); i++)
+        {
+            if (rows[i].Length != 0)
+            {
+                rows[i].Trim();
+                var splitComma = rows[i].Split(",");
+                classList.Add((splitComma[0], splitComma[1], splitComma[2], splitComma[3], splitComma[4], splitComma[5]));
+            }
+        }
+        return classList;
+    }
+}
+
